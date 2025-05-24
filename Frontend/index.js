3c242c2593c49f3c7237408f146514d568ccba3d
manikant21 <manikant.hit.ece23@heritageit.edu.in>
2025-05-24 18:24:27 +0530

    login screen

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,10 +1,30 @@
 const BASE_URL_USER = "http://localhost:3000/api/v1/user";
-const form = document.querySelector("form");
+const form = document.getElementById("register_form");
 const name = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
+const login_form = document.getElementById("login_form");
+const email_login = document.getElementById("email_login");
+const password_login = document.getElementById("password_login");
 
+if(login_form) {
+login_form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (email_login.value == "" || password_login.value == "") {
+        alert("Please fill all details");
+        return
+    }
+    const loginDetails = {
+        email: email_login.value,
+        password: password_login.value
+    }
+    console.log(loginDetails);
+
+})
+}
+
+if(form){
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     if (name.value == "" || email.value == "" || password.value == "") {
@@ -38,3 +58,4 @@ form.addEventListener('submit', async (event) => {
 
 
 })
+}
